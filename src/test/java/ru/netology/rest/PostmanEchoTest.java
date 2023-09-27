e8c7966855a90ab0e827554efdff41e2c6472c7e
Zvyagintsev Sergey <zsv069@gmail.com>
2023-09-27 21:58:45 +0300

    Fail test2

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("SO HARD STUDY"))
+                .body("data",equalTo("SO HARD"))
         ;
     }
 }
